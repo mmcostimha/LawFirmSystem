c78889aacced07e01f2d465438880808638f83c3
mmcostimha <165158380+mmcostimha@users.noreply.github.com>
2025-09-30 10:09:13 +0100

    Initial commit

---
+++ LawFirmAPI/src/main/java/com/example/LawFirmAPI/LawFirmApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.LawFirmAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawFirmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawFirmApiApplication.class, args);
+	}
+
+}
---
+++ LawFirmAPI/src/test/java/com/example/LawFirmAPI/LawFirmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LawFirmAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawFirmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
